65b576d7799986c8a5719812343c3dec4af123fb
Andrianna <andrianna.odell@gmail.com>
2022-06-29 16:36:18 -0400

    commit tree ccee5cc645b4f9a2d6c840bc7de30846a4d12207

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Write your solution in this file!
+
+// define employee
+const employee = {
+    name : "Bob",
+    address: "11 Broadway",
+};
+
+// reset or destructive situations
+beforeEach(function () {
+    for(const key in employee){
+        delete employee[key];
+    }
+    return employee.name = 'Sam';
+});
+
+// create function that will return an employee with the original key 
+// value pairs and the new key value pair
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const newEmployee = {...employee};
+    newEmployee[key] = value;
+
+    return newEmployee;
+}
+
+// destructively update the employee object
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee;
+}
+
+// non destructive deletion of employee
+function deleteFromEmployeeByKey(employee, key){
+    const newEmployee = {...employee};
+    delete newEmployee[key];
+    return newEmployee;
+
+}
+
+// destructive deletion from employer
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    const newEmployee = employee;
+    delete newEmployee.name;
+    return newEmployee;
+}
+
